75a63685f8a76b66c93be6564c88f4c93a311d02
ddrabic <ddrabic@gmail.com>
2026-09-13 21:33:00 +0200

    20260913b

--- lib/config.php
+++ lib/config.php
@@ -1,9 +1,7 @@
 <?php
 
 declare(strict_types=1);
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
 use Upp\Config\Config;
 
 function upp_config(?string $key = null, mixed $default = null): mixed
